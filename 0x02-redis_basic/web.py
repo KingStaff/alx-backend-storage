235bc5913649a54892f2647d1a4ba9850fcf0743
kingStaff <karimawudulai@gmail.com>
2024-01-24 14:18:17 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,15 +6,18 @@ import requests
 from functools import wraps
 from typing import Callable
 
-# Module-level Redis instance
+
 redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
 
 def data_cacher(method: Callable) -> Callable:
-    '''Decorator to cache the output of fetched data and track requests.
+    '''Caches the output of fetched data.
     '''
     @wraps(method)
-    def cached_page_request(url: str) -> str:
-        '''Wrapper function for caching the output.
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
         '''
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
@@ -24,8 +27,8 @@ def data_cacher(method: Callable) -> Callable:
         redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
+    return invoker
 
-    return cached_page_request
 
 @data_cacher
 def get_page(url: str) -> str:
